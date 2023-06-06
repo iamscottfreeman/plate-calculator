8a1345e0996ce723f3f4ef22ccbc259b7cf3c6eb
Scott Freeman <git@scottfreeman.net>
2023-06-06 16:34:11 +0100

    Added 15kg bar

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -14,7 +14,21 @@ const Bar = ({ barWeight, collars, setBarWeight, setCollars }: Props) => {
       <div className={`uppercase text-left text-xl ${fonts.header.className}`}>Bar</div>
       <div className='flex justify-between align-middle'>
         <label
-          className={`flex items-center bg-gray-800 text-white border-2 px-8 py-4 cursor-pointer select-none ${
+          className={`flex items-center bg-gray-800 text-white border-2 px-4 py-4 mr-2 cursor-pointer select-none ${
+            barWeight === 15 ? 'border-red-600 bg-gray-700' : 'text-gray-300 border-gray-700'
+          }`}
+        >
+          <input
+            type='radio'
+            name='barWeight'
+            className='hidden'
+            checked={barWeight === 15}
+            onChange={() => setBarWeight(15)}
+          />
+          15kg
+        </label>
+        <label
+          className={`flex items-center bg-gray-800 text-white border-2 px-4 py-4 mr-2 cursor-pointer select-none ${
             barWeight === 20 ? 'border-red-600 bg-gray-700' : 'text-gray-300 border-gray-700'
           }`}
         >
@@ -28,7 +42,7 @@ const Bar = ({ barWeight, collars, setBarWeight, setCollars }: Props) => {
           20kg
         </label>
         <label
-          className={`flex items-center bg-gray-800 text-white border-2 px-8 py-4 mx-2 cursor-pointer select-none ${
+          className={`flex items-center bg-gray-800 text-white border-2 px-4 py-4 mr-2 cursor-pointer select-none ${
             barWeight === 25 ? 'border-red-600 bg-gray-700' : 'text-gray-300 border-gray-700'
           }`}
         >
@@ -41,9 +55,8 @@ const Bar = ({ barWeight, collars, setBarWeight, setCollars }: Props) => {
           />
           25kg
         </label>
-
         <label
-          className={`block bg-gray-800 text-white border-2 px-8 py-2 cursor-pointer select-none ${
+          className={`block bg-gray-800 text-white border-2 px-4 py-2 cursor-pointer ${
             collars ? 'border-red-600 bg-gray-700' : 'text-gray-300 border-gray-700'
           }`}
         >
