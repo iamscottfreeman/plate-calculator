0bf758a2d969b8e7e9f4dc5787c16d939ab6beff
Scott Freeman <git@scottfreeman.net>
2023-06-06 18:05:03 +0100

    UI Tweaks

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -56,7 +56,7 @@ const Bar = ({ barWeight, collars, setBarWeight, setCollars }: Props) => {
           25kg
         </label>
         <label
-          className={`block bg-gray-800 text-white border-2 px-4 py-2 cursor-pointer rounded ${
+          className={`block bg-gray-800 text-white border-2 px-4 py-2 cursor-pointer select-none rounded ${
             collars ? 'border-red-600 bg-gray-700' : 'text-gray-300 border-gray-700'
           }`}
         >
