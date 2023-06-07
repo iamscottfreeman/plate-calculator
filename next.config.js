4d10e2f17faddbaab3774ae0dd6cee8222fe7dc9
Scott Freeman <git@scottfreeman.net>
2023-06-07 22:27:57 +0100

    PWA

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('@imbios/next-pwa')({
+  dest: 'public'
+});
+
 const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
