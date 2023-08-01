1ba954f3e0445a46dc62227a47ad497ff1d47516
Scott Freeman <email@scottfreeman.net>
2023-08-01 11:49:22 +0100

    OG metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
+import { Metadata } from 'next';
 
 import './globals.css';
 import { fonts } from './fonts';
@@ -6,10 +7,19 @@ import { fonts } from './fonts';
 const APP_NAME = 'Plate Calculator';
 const APP_DESC = 'Quick and easy plate calculator for Powerlifting';
 
-export const metadata = {
+export const metadata: Metadata = {
   title: APP_NAME,
   description: APP_DESC,
   applicationName: APP_NAME,
+  openGraph: {
+    images: [
+      {
+        url: 'https://plates.scottfreeman.net/icons/512.png',
+        width: 512,
+        height: 512
+      }
+    ]
+  },
   appleWebApp: {
     capable: true,
     title: APP_NAME,
